59e6bf71174a389082bc76de28b4b3bf208775ba
nirmitipatil <91395646+nirmitipatil@users.noreply.github.com>
2023-06-17 17:47:38 -0400

    Update deploy.js

--- v1.0/scripts/deploy.js
+++ v1.0/scripts/deploy.js
@@ -1,1 +1,16 @@
+async function main() {
+  // Deploying the BasicDutchAuction contract
+  const BasicDutchAuction = await ethers.getContractFactory("BasicDutchAuction");
+  const basicDutchAuction = await BasicDutchAuction.deploy();
 
+  await basicDutchAuction.deployed();
+
+  console.log("BasicDutchAuction deployed to:", basicDutchAuction.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
